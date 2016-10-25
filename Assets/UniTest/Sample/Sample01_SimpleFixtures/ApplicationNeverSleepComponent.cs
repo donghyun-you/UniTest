08a2eda483570fced70e229c951b34eaf3ea518c
Donghyun-You <ruel.o.wisp@gmail.com>
2016-10-25 21:03:27 +0900

    make unitest runner app do not off the screen

---
+++ Assets/UniTest/Sample/Sample01_SimpleFixtures/ApplicationNeverSleepComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UniTest 
+{
+	public class ApplicationNeverSleepComponent : MonoBehaviour 
+	{
+		void Start () 
+		{
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		}
+	}
+}
